8bc8a36eb8e4c5b0e885454e3389c5cbecbf249a
eseverett <eseveret@asu.edu>
2024-05-11 22:48:33 -0700

    LCD display works

--- src/servo_control_with_joystick/servo_control_with_joystick.ino
+++ src/servo_control_with_joystick/servo_control_with_joystick.ino
@@ -1,23 +1,27 @@
 #include <Servo.h>
+#include <LiquidCrystal.h>
 
 Servo servo;
-
-int pos = 0; // global position variable
-const int servo_signal_pin = 9; // pulse signal source pin
+int pos = 0; 
+const int servo_signal_pin = 9;
 const int x_axis_pin = A0;
-const int y_axis_pin = A1;
-int x_value = 0;
-int y_value = 0;
 const int switchPin = 6;
-int lastButtonState = LOW; 
+int lastButtonState = HIGH; // Changed to HIGH for INPUT_PULLUP
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 bool buttonPressed = false;
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() {
   servo.attach(servo_signal_pin);
-  pinMode(switchPin, INPUT); 
+  pinMode(switchPin, INPUT_PULLUP); 
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("- Starting -");
+  delay(2000);  // Delay to read the start message
+  lcd.clear();
 }
 
 void loop() {
@@ -38,23 +42,34 @@ void loop() {
   }
   
   lastButtonState = reading;
+
+  static int lastPos = -1; // Store last position to minimize LCD updates
+  if (lastPos != pos) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pos: ");
+    lcd.print(pos);
+    lcd.setCursor(0, 1);
+    lastPos = pos;
+  }
+
   delay(10); 
 }
 
 void move_servo() {
-  x_value = analogRead(x_axis_pin);
+  int x_value = analogRead(x_axis_pin);
   pos = (int)x_value / 5.667;
   servo.write(pos);
 }
 
 void button_click() {
   // testing sweep
-  for (pos = 0; pos <= 180; pos += 1) {
-    servo.write(pos);
+  for (int tempPos = 0; tempPos <= 180; tempPos += 1) {
+    servo.write(tempPos);
     delay(15);
   }
-  for (pos = 180; pos >= 0; pos -= 1) {
-    servo.write(pos);
+  for (int tempPos = 180; tempPos >= 0; tempPos -= 1) {
+    servo.write(tempPos);
     delay(15);
   }
 }
