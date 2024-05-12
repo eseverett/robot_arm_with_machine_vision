24508d305991a2b5eec0e094d35ea2bbf98fc89c
eseverett <eseveret@asu.edu>
2024-05-11 20:14:38 -0700

    joystick controls servo motor

--- src/servo_control_with_joystick/servo_control_with_joystick.ino
+++ src/servo_control_with_joystick/servo_control_with_joystick.ino
@@ -2,14 +2,58 @@
 
 Servo servo;
 
+int pos = 0; // global position variable
+const int servo_signal_pin = 9; // pulse signal source pin
 
+const int x_axis_pin = A0;
+const int y_axis_pin = A1;
+int x_value = 0;
+int y_value = 0;
+const int switchPin = 6; 
+int button_pressed = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  servo.attach(servo_signal_pin);
+
+  pinMode(switchPin, INPUT);
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  move_servo();
 
+  if (digitalRead(switchPin)) {
+    button_click();
+  }
+
+  Serial.println(pos);
+  delay(100);
 }
+
+void move_servo() {
+  x_value = analogRead(x_axis_pin);
+  pos = (int) x_value / 5.667;
+  servo.write(pos);
+}
+
+void button_click() {
+  for (pos = 0; pos <= 180; pos += 1) { 
+
+    servo.write(pos);              
+
+    delay(15);                       
+
+  }
+
+  for (pos = 180; pos >= 0; pos -= 1) { 
+
+    servo.write(pos);              
+
+    delay(15);                       
+
+  }
+}
+
