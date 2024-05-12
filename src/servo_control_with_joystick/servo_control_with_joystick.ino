ed44c5cc1389b9ee96bda2ed7cc27be58f8738d8
eseverett <eseveret@asu.edu>
2024-05-11 21:43:53 -0700

    joystick button press works

--- src/servo_control_with_joystick/servo_control_with_joystick.ino
+++ src/servo_control_with_joystick/servo_control_with_joystick.ino
@@ -4,56 +4,57 @@ Servo servo;
 
 int pos = 0; // global position variable
 const int servo_signal_pin = 9; // pulse signal source pin
-
 const int x_axis_pin = A0;
 const int y_axis_pin = A1;
 int x_value = 0;
 int y_value = 0;
-const int switchPin = 6; 
-int button_pressed = 0;
+const int switchPin = 6;
+int lastButtonState = LOW; 
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+bool buttonPressed = false;
 
 void setup() {
-  // put your setup code here, to run once:
   servo.attach(servo_signal_pin);
-
-  pinMode(switchPin, INPUT);
+  pinMode(switchPin, INPUT); 
   Serial.begin(9600);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   move_servo();
+  int reading = digitalRead(switchPin);
 
-  if (digitalRead(switchPin)) {
-    button_click();
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
   }
 
-  Serial.println(pos);
-  delay(100);
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonPressed) {
+      buttonPressed = reading;
+      if (buttonPressed == LOW) { // Check for button press (button is active low)
+        button_click();
+      }
+    }
+  }
+  
+  lastButtonState = reading;
+  delay(10); 
 }
 
 void move_servo() {
   x_value = analogRead(x_axis_pin);
-  pos = (int) x_value / 5.667;
+  pos = (int)x_value / 5.667;
   servo.write(pos);
 }
 
 void button_click() {
-  for (pos = 0; pos <= 180; pos += 1) { 
-
-    servo.write(pos);              
-
-    delay(15);                       
-
+  // testing sweep
+  for (pos = 0; pos <= 180; pos += 1) {
+    servo.write(pos);
+    delay(15);
   }
-
-  for (pos = 180; pos >= 0; pos -= 1) { 
-
-    servo.write(pos);              
-
-    delay(15);                       
-
+  for (pos = 180; pos >= 0; pos -= 1) {
+    servo.write(pos);
+    delay(15);
   }
 }
-
